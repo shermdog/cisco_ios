4343f5fdf65dd969f495927910a5c6c139479d3f
Rick Sherman <rick@shermdog.com>
2019-03-18 15:37:13 -0500

    (poc) CLI Command Task This commit is a POC for a CLI Command Task.  It currently requires enable mode and doesn't have any real guardrails
    ```
    [root@puppet tasks]# puppet task run cisco_ios::cli_command command='sh interfaces vlan 1' target=ios -n puppet
    Starting job ...
    Note: The task will run only on permitted nodes.
    New job ID: 9
    Nodes: 1
    
    Started on puppet ...
    Finished on node puppet
      status : success
      results : sh interfaces vlan 1
    Vlan1 is up, line protocol is up
      Hardware is Ethernet SVI, address is 5254.00d7.8001 (bia 5254.00d7.8001)
      Internet address is 192.168.122.254/24
      MTU 1500 bytes, BW 1000000 Kbit/sec, DLY 10 usec,
         reliability 255/255, txload 1/255, rxload 1/255
      Encapsulation ARPA, loopback not set
      Keepalive not supported
      ARP type: ARPA, ARP Timeout 04:00:00
      Last input 00:00:00, output never, output hang never
      Last clearing of "show interface" counters never
      Input queue: 0/75/0/0 (size/max/drops/flushes); Total output drops: 0
      Queueing strategy: fifo
      Output queue: 0/40 (size/max)
      5 minute input rate 0 bits/sec, 0 packets/sec
      5 minute output rate 0 bits/sec, 0 packets/sec
         1567 packets input, 130092 bytes, 0 no buffer
         Received 0 broadcasts (0 IP multicasts)
         0 runts, 0 giants, 0 throttles
         0 input errors, 0 CRC, 0 frame, 0 overrun, 0 ignored
         1212 packets output, 179551 bytes, 0 underruns
         0 output errors, 0 interface resets
         0 unknown protocol drops
         0 output buffer failures, 0 output buffers swapped out
    ios-local-1#
    
    Job completed. 1/1 nodes succeeded.
    ```

---
+++ tasks/cli_command.rb
@@ -0,0 +1,105 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+
+require 'json'
+require 'puppet'
+require 'puppet/util/network_device/config'
+require 'timeout'
+
+def require_module_file(file)
+  require "#{Puppet[:plugindest]}/#{file}"
+end
+
+# PARAMETERS: JSON object from STDIN with the following fields:
+#
+# - target: a required string parameter.
+# - noop: an optional boolean metaparameter, will set params['_noop'].
+
+# Read parameters, set defaults, and validate values.
+
+def read_parameters
+  params = read_stdin
+  return_error("Parameter 'target' contains illegal characters") unless safe_string?(params['target'])
+  params
+end
+
+# Read parameters as JSON from STDIN.
+
+def read_stdin
+  params = {}
+  begin
+    Timeout.timeout(3) do
+      params = JSON.parse(STDIN.read)
+    end
+  rescue Timeout::Error
+    return_error('Cannot read parameters as JSON from STDIN')
+  end
+  params
+end
+
+# Validate strings. While handled externally, validate internally for defense in depth.
+
+def safe_string?(param)
+  return true unless param
+  (param =~ %r{^[A-Za-z0-9._-]+$}) != nil
+end
+
+# Return an error and exit.
+
+def return_error(message)
+  result = {}
+  result[:_error] = {
+    msg:     message,
+    kind:    'puppetlabs/cisco_ios',
+    details: {},
+  }
+  puts result.to_json
+  exit 1
+end
+
+# Return a result and exit.
+
+def return_success(message)
+  result = {}
+  result[:status]  = 'success'
+  result[:results] = message
+  puts result.to_json
+  exit 0
+end
+
+# Execute the task.
+
+params = read_parameters
+
+# Allow this task to access configuration settings, such as: Puppet[:environment].
+
+Puppet.initialize_settings
+
+# Identify the target device and its URL.
+
+devices = Puppet::Util::NetworkDevice::Config.devices.dup
+devices.select! { |key, _value| key == params['target'] }
+if devices.empty?
+  return_error("device conf error: unable to find target device #{params['target']} in #{Puppet[:deviceconfig]}")
+end
+target_device_url = URI.parse(devices[params['target']].url).to_s
+if target_device_url.empty?
+  return_error("device conf error: unable to find URL for target device #{params['target']} in #{Puppet[:deviceconfig]}")
+end
+
+# Honor the 'noop' parameter.
+
+if params['_noop'] == true
+  return_success("noop: true, target: #{params['target']}")
+end
+
+# Save.
+
+begin
+  require_module_file('puppet/util/network_device/cisco_ios/device')
+  cisco_ios_device = Puppet::Util::NetworkDevice::Cisco_ios::Device.new(target_device_url)
+  result = cisco_ios_device.run_command_enable_mode(params['command'])
+  return_success("#{result}")
+rescue StandardError => e
+  cli_error = e.message
+  return_error(cli_error)
+end
